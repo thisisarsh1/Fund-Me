5e9ecdc29ef7ce590580bb06d2b4e52060a5ccdd
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-09-14 23:42:08 +0530

    fixed issues

--- client/src/app/(routes)/CreateCampaign/page.jsx
+++ client/src/app/(routes)/CreateCampaign/page.jsx
@@ -1,9 +1,142 @@
-import React from 'react'
-
+"use client"
+import React, { useState } from 'react'
+import { HandCoins } from 'lucide-react';
 function page() {
+  const [form,setForm]=useState({
+    name:'',
+    title:'',
+    description:'',
+    target:'',
+    deadline:'',
+    image:''
+  })
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+    console.log(form);
+  };
   return (
     <div className='p-2 md:p-10'>
-        create campaign
+        <div className=' min-h-screen rounded-2xl p-2'>
+            <div className="flex flex-col items-center ">
+              <p className="text-6xl lg:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-600 py-4 sm:py-8">
+                Start A Campaign
+              </p>
+            </div>
+
+
+            <form 
+        className=" p-8 rounded-lg shadow-md max-w-full w-full"
+        onSubmit={handleSubmit}
+      >
+       
+        
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {/* Name */}
+          <div className="col-span-1">
+            <label className="block mb-2 text-sm font-medium text-gray-700">Your Name</label>
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-white bg-neutral-900 text-neutral-500 border-neutral-700"
+              placeholder="Enter your name"
+            />
+          </div>
+
+          {/* Title */}
+          <div className="col-span-1">
+            <label className="block mb-2 text-sm font-medium text-gray-700">Title</label>
+            <input
+              type="text"
+              name="title"
+              value={form.title}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-white bg-neutral-900 text-neutral-500 border-neutral-700"
+              placeholder="Campaign title"
+            />
+          </div>
+
+          {/* Description */}
+          <div className="col-span-2">
+            <label className="block mb-2 text-sm font-medium text-gray-700">Story</label>
+            <textarea
+              name="description"
+              value={form.description}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none  focus:border-white bg-neutral-900 text-neutral-500 border-neutral-700"
+              placeholder="Write your Story"
+              rows="6"
+            />
+          </div>
+
+          <div className='col-span-2 h-[20vh] bg-neutral-400 rounded-2xl text-neutral-900 flex justify-center items-center'>
+          <HandCoins size={50} className="text-neutral-900" />
+          <div className='text-4xl font-bold'>YOU WILL GET 100% OF THE RAISED AMOUNT !
+            </div>
+          </div>
+
+          {/* Target */}
+          <div className="col-span-1">
+            <label className="block mb-2 text-sm font-medium text-gray-700">Target Amount</label>
+            <input
+              type="number"
+              name="target"
+              value={form.target}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none  focus:border-white bg-neutral-900 text-neutral-500 border-neutral-700"
+              placeholder="Target amount"
+            />
+          </div>
+
+          {/* Deadline */}
+          <div className="col-span-1">
+            <label className="block mb-2 text-sm font-medium text-gray-700">Deadline</label>
+            <input
+              type="date"
+              name="deadline"
+              value={form.deadline}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none  focus:border-white bg-neutral-900 text-neutral-500 border-neutral-700"
+            />
+          </div>
+
+          {/* Image */}
+          <div className="col-span-2">
+            <label className="block mb-2 text-sm font-medium text-gray-700">Campaign Image</label>
+            <input
+              type="url"
+              name="image"
+              value={form.image}
+              onChange={handleChange}
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none  focus:border-white bg-neutral-900 text-neutral-500 border-neutral-700"
+              placeholder="Image URL"
+            />
+          </div>
+        </div>
+
+        {/* Submit Button */}
+        <div className="mt-6 text-center">
+        <button 
+        type='submit'
+        className="h-12 animate-shimmer items-center rounded-md border border-neutral-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors hover:text-white ">
+      Submit
+        </button>
+        </div>
+      </form>
+
+
+        </div>
+        
     </div>
     
   )
---
+++ client/src/app/layout.jsx
@@ -0,0 +1,156 @@
+"use client";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThirdwebProvider } from "thirdweb/react";
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "@/app/components/ui/sidebar";
+import { Grid2X2 } from 'lucide-react';
+import { Send } from 'lucide-react';
+import { Bitcoin } from 'lucide-react';
+import { ReceiptIndianRupee } from 'lucide-react';
+import {
+  IconArrowLeft,
+  IconUserBolt,
+} from "@tabler/icons-react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+const inter = Inter({ subsets: ["latin"] });
+
+const links = [
+  {
+    label: "Home",
+    href: "/",
+    icon: (
+      <Grid2X2 className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
+  },
+  {
+    label: "Create Campaign",
+    href: "/CreateCampaign",
+    icon: (
+      <Send className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
+  },
+  {
+    label: "Payments",
+    href: "/Payments",
+    icon: (
+      <Bitcoin className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
+  },
+  {
+    label: "Withdraw",
+    href: "/Withdraw",
+    icon: (
+      <ReceiptIndianRupee className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
+  },
+  {
+    label: "Profile",
+    href: "/Profile",
+    icon: (
+      <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
+  },
+  {
+    label: "Logout",
+    href: "#",
+    icon: (
+      <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+    ),
+  },
+];
+
+export default function RootLayout({ children }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className=" overflow-x-hidden">
+          <ThirdwebProvider>
+            <div
+              className={cn(
+                "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-screen flex-1 max-w-8xl mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+                "min-h-screen sticky"
+              )}
+            >
+              <Sidebar open={open} setOpen={setOpen}>
+                <SidebarBody className="justify-between gap-10 h-[98vh] ">
+                  <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden fixed">
+                    {open ? <Logo /> : <LogoIcon />}
+                    <div className="mt-8 flex flex-col gap-2">
+                      {links.map((link, idx) => (
+                        <SidebarLink key={idx} link={link} />
+                      ))}
+                    </div>
+                  </div>
+                  <div className="flex flex-col justify-end h-[95vh] fixed">
+                  <div className="  h-7 w-7  rounded-full ">
+                    <SidebarLink
+                      link={{
+                        label: "Rehbar Khan",
+                        href: "https://github.com/thisisarsh1",
+                        icon: (
+                          <div className="h-7 w-7 flex-shrink-0">
+                            <Image
+                              src="https://avatars.githubusercontent.com/u/136853370?s=400&u=8808221856e366b62f6d0c83cbef90aa793e89d5&v=4"
+                              width={28}
+                              height={28}
+                              alt="Avatar"
+                              style={{ borderRadius: "50%" }} // Use inline style for the round avatar
+                            />
+                          </div>
+                        ),
+                      }}
+                    />
+                  </div>
+                  </div>
+                  
+                </SidebarBody>
+              </Sidebar>
+
+              <div className="flex flex-1 overflow-y-auto">
+                <div className="p-1 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </ThirdwebProvider>
+        </main>
+      </body>
+    </html>
+  );
+}
+
+export const Logo = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre"
+      >
+        Fund-Me
+      </motion.span>
+    </Link>
+  );
+};
+
+export const LogoIcon = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+    </Link>
+  );
+};
